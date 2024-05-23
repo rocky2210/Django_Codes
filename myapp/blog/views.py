a4c72e02cc2f0b43cdf8084a9b2996788d6869cf
rocky2210 <lilavathra@gmail.com>
2024-05-23 11:52:26 +0530

    DJ-3 :More Urls & Views

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world, You are at blog's index")
+
+def detail(request):
+    return HttpResponse("You are viewing post detail page")
--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
