ae73a1c20f1ed7912978577475fb2f36f79bbc17
rocky2210 <lilavathra@gmail.com>
2024-05-23 16:19:16 +0530

    DJ-7 :Rendering html

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -5,10 +5,10 @@ from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world, You are at blog's index")
+    return render(request,"blog/index.html")
 
 def detail(request,post_id):
-    return HttpResponse(f"You are viewing post detail page and Id is {post_id}")
+    return render(request,"blog/detail.html")
 
 def old_url_redirect(request):
     return redirect(reverse('blog:new_page_url'))
