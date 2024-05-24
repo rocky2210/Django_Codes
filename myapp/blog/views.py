50bf8c9e13bc0feed57877d3643cda1308e30f00
rocky2210 <lilavathra@gmail.com>
2024-05-24 17:04:47 +0530

    DJ-22 :  getting post data

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -2,19 +2,22 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
 import logging
+from .models import Post
 
 # Create your views here.
-
-posts = [
-        {'id':1,'title':'post 1', 'content':'content of Post 1'},
-        {'id':2,'title':'post 2', 'content':'content of Post 2'},
-        {'id':3,'title':'post 3', 'content':'content of Post 3'},
-        {'id':4,'title':'post 4', 'content':'content of Post 4'}
-    ]
+# dummy data
+# posts = [
+#         {'id':1,'title':'post 1', 'content':'content of Post 1'},
+#         {'id':2,'title':'post 2', 'content':'content of Post 2'},
+#         {'id':3,'title':'post 3', 'content':'content of Post 3'},
+#         {'id':4,'title':'post 4', 'content':'content of Post 4'}
+#     ]
     
     
 def index(request):
     blog_title = "Latest Posts !"
+    # Getting data from the post mode
+    posts = Post.objects.all()
     return render(request,"blog/index.html",{'blog_title':blog_title,'posts':posts})
 
 def detail(request,post_id):
