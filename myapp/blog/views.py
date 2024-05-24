7eb0603a1d8993ddb2690318059ffd6d83682569
rocky2210 <lilavathra@gmail.com>
2024-05-24 05:36:22 +0530

    DJ-17 :using logger for debugging

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
-
+import logging
 
 # Create your views here.
-def index(request):
-    blog_title = "Latest Posts !"
-    posts = [
+
+posts = [
         {'id':1,'title':'post 1', 'content':'content of Post 1'},
         {'id':2,'title':'post 2', 'content':'content of Post 2'},
         {'id':3,'title':'post 3', 'content':'content of Post 3'},
         {'id':4,'title':'post 4', 'content':'content of Post 4'}
     ]
+    
+    
+def index(request):
+    blog_title = "Latest Posts !"
     return render(request,"blog/index.html",{'blog_title':blog_title,'posts':posts})
 
 def detail(request,post_id):
-    return render(request,"blog/detail.html")
+    post = next((item for item in posts if item['id'] == int(post_id)),None)
+    # logger = logging.getLogger('Testing')
+    # logger.debug(f'post variable is {post}')
+    return render(request,"blog/detail.html",{'post':post})
 
 def old_url_redirect(request):
     return redirect(reverse('blog:new_page_url'))
