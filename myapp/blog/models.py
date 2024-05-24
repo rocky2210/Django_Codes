abcde81cd318b70ace61e0e2d652a0e2ac7c9e7e
rocky2210 <lilavathra@gmail.com>
2024-05-24 16:33:56 +0530

    DJ-20 : creating migrations

--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -5,7 +5,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    img_url = models.URLField(blank=True)
+    img_url = models.URLField(null=True)
+    created_at  = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.title
