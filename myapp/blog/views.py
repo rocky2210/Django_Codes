54f68ad625efdd5e7338c7b79a77c426545ca37b
rocky2210 <lilavathra@gmail.com>
2025-01-20 06:26:35 +0000

    Added view and url

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World, you are at blog's index")
+
+def detail(request):
+    return HttpResponse("You are viewing post detail page")
