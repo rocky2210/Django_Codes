23581c13a7a09ef7f5361c0c6c5fc515d8ed18a7
rocky2210 <lilavathra@gmail.com>
2025-01-20 10:03:31 +0000

    Reverse function and named url

--- myapp/blog/urls.py
+++ myapp/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views 
+
+app_name = 'blog'  # namespace
+
 urlpatterns = [
     path("", views.index,name="index"),
     path("post/<int:post_id>", views.detail,name="detail"),
-    path("new_url", views.new_url_view,name="new_url"),
+    path("new_url", views.new_url_view,name="new_page_url"),
     path("old_url", views.old_url_redirect,name="old_url"),
     
 ]
--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -9,7 +10,7 @@ def detail(request,post_id):
     return HttpResponse(f"You are viewing post detail page. And Id is {post_id}")
 
 def old_url_redirect(request):
-    return redirect("new_url")
+    return redirect(reverse('blog:new_page_url'))
 
 def new_url_view(request):
     return HttpResponse("This is the new url")
