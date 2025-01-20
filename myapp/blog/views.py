38f8cee2d7cb6bed23b9af61a1dd631429693fdd
rocky2210 <lilavathra@gmail.com>
2025-01-20 09:24:34 +0000

    Redirecting url

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
@@ -7,3 +7,10 @@ def index(request):
 
 def detail(request,post_id):
     return HttpResponse(f"You are viewing post detail page. And Id is {post_id}")
+
+def old_url_redirect(request):
+    return redirect("new_url")
+
+def new_url_view(request):
+    return HttpResponse("This is the new url")
+
