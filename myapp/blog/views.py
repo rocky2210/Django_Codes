cf95a384d2ab63ac4b2c6e90e5df19c096df8179
rocky2210 <lilavathra@gmail.com>
2025-01-21 07:04:50 +0000

    Static files

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -4,10 +4,10 @@ from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World, you are at blog's index")
+    return render(request,'blog/index.html')
 
 def detail(request,post_id):
-    return HttpResponse(f"You are viewing post detail page. And Id is {post_id}")
+    return render(request,'blog/detail.html')
 
 def old_url_redirect(request):
     return redirect(reverse('blog:new_page_url'))
