c3a56f915c9047860f8b56b5a9c10219556fd527
rocky2210 <lilavathra@gmail.com>
2025-02-25 08:00:04 +0000

    Filter and For Tag

--- myapp/blog/views.py
+++ myapp/blog/views.py
@@ -5,7 +5,15 @@ from django.urls import reverse
 # Create your views here.
 def index(request):
     blog_title = "Latest Posts"
-    return render(request,'blog/index.html',{'blog_title': blog_title})
+    posts = [
+        {"title":"Post 1",'content': 'content of post 1'},
+        {"title":"Post 2",'content': 'content of post 2'},
+        {"title":"Post 3",'content': 'content of post 3'},
+        {"title":"Post 4",'content': 'content of post 4'},
+        {"title":"Post 4",'content': ''},
+    ]
+    
+    return render(request,'blog/index.html',{'blog_title': blog_title,'posts': posts})
 
 def detail(request,post_id):
     return render(request,'blog/detail.html')
